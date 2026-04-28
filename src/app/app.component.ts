f213465552d64f7ea27e8cb2753a1863e251b10c
Festik <georg.tihonov227@gmail.com>
2026-04-28 13:30:24 +0200

    replace slide animation with fade on route transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import {IonApp, IonRouterOutlet} from '@ionic/angular/standalone';
+import { AnimationController } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  standalone: true,
+  imports: [IonRouterOutlet, IonApp]
 })
 export class AppComponent {
-  constructor() {}
+
+  constructor(private animationCtrl: AnimationController) {}
+
+  fadeAnimation = (baseEl: any, opts: any) => {
+    const enterAnim = this.animationCtrl
+      .create()
+      .addElement(opts.enteringEl)
+      .duration(200)
+      .fromTo('opacity', 0, 1);
+
+    const leaveAnim = this.animationCtrl
+      .create()
+      .addElement(opts.leavingEl)
+      .duration(200)
+      .fromTo('opacity', 1, 0);
+
+    return this.animationCtrl
+      .create()
+      .addAnimation([enterAnim, leaveAnim]);
+  };
 }
